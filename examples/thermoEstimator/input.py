be24e1411b7388ee63ad0e801a997499f7e61cf1
KEHANG <kehanghan@gmail.com>
2016-03-01 12:34:54 -0500

    added a input for running pm7 for s1_3_6 branch

--- examples/thermoEstimator/input.py
+++ examples/thermoEstimator/input.py
@@ -3,29 +3,29 @@ database(
 )
 
 species(
-    label='DIPK',
-    structure=SMILES("CC(C)C(=O)C(C)C"),
+    label='s1_3_6_ane',
+    structure=SMILES("C1CCC2(CC1)CC2"),
 )
 species(
-    label='O2',
-    structure=SMILES("[O][O]"),
+    label='s1_3_6_ene_1',
+    structure=SMILES("C1=CC2(CCC1)CC2"),
 )
 species(
-    label='R_tert',
-    structure=SMILES("CC(C)C(=O)[C](C)C"),
+    label='s1_3_6_ene_2',
+    structure=SMILES("C1=CCC2(CC1)CC2"),
 )
 species(
-    label='R_pri',
-    structure=SMILES("CC(C)C(=O)C(C)[CH2]"),
+    label='s1_3_6_diene_1_4',
+    structure=SMILES("C1=CC2(C=CC1)CC2"),
 )
 species(
-    label='Cineole',
-    structure=SMILES('CC12CCC(CC1)C(C)(C)O2'),
+    label='s1_3_6_diene_1_3',
+    structure=SMILES("C1C=CC2(CC=1)CC2"),
 )
 
 quantumMechanics(
     software='mopac',
-    method='pm3',
+    method='pm7',
     onlyCyclics = True,
     maxRadicalNumber = 0,
 )
